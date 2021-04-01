c2f828a9c6770f03effe4d63f5dced71e907fb31
sam <sblan45@gmail.com>
2021-04-01 14:49:18 -0400

    adds d3 json syntax to load in data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { json } from 'd3'
 
 import ChartWrapper from './ChartWrapper';
 
 class App extends Component {
+  state = {
+    data: []
+  }
+  
+  componentDidMount() {
+    json("http://udemy-react-d3.firebaseio.com/children.json")
+      .then( response => {
+        this.setState({
+          data: response
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  renderChart() {
+    if (this.state.data.length == 0) {
+      return "No data yet"
+    }
+    return <ChartWrapper data={this.state.data}/>
+  }
+  
   render() {
     return (
       <div>
@@ -15,7 +37,7 @@ class App extends Component {
         </Navbar>
         <Container>
           <Row>
-            <Col md={6} xs={12}><ChartWrapper /></Col>
+            <Col md={6} xs={12}>{this.renderChart()}</Col>
             <Col md={6} xs={12}></Col>
           </Row>
         </Container>
--- src/ChartWrapper.js
+++ src/ChartWrapper.js
@@ -1,13 +1,13 @@
 import React, { useRef, useState, useEffect } from 'react';
 import D3Chart from './D3Chart';
 
-const ChartWrapper = () => {
+const ChartWrapper = ({data}) => {
 	const chartArea = useRef(null)
 	const [chart, setChart] = useState(null)
 
 	useEffect(() => {
 		if (!chart) {
-			setChart(new D3Chart(chartArea.current))
+			setChart(new D3Chart(chartArea.current, data))
 		}
 		else {
 			chart.update()
